da21a32db5a0c8f7b80e4f656d75929d16fa5446
datnguyen130 <65001229+datnguyen130@users.noreply.github.com>
2020-10-17 18:00:58 +0700

    lab 6
    lab 6

---
+++ java/lab_06/Ex3/Person.java
@@ -0,0 +1,31 @@
+package lab_06.Ex3;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(){
+        this.name = "Student";
+        this.address = "Ha Noi";
+    }
+
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String toString(){
+        return this.name + "(" + this.address + ")";
+    }
+}
