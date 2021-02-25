84d26604088896c74b80fcd667a1bfe9f0ebe7e7
datnguyen130 <65001229+datnguyen130@users.noreply.github.com>
2021-02-25 14:11:47 +0700

    lab 06
    lab 06

---
+++ PHP/laravel-tutorial/routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Homecontroller;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/',[Homecontroller::class,'showwelcome']);
+Route::get('about', function (){
+   return "About content";
+});
+Route::get('about/direction', function (){
+   return "Direction go here";
+});
+//Route::any('submit-form',function (){
+//    return "Test Routes any";
+//});
+Route::get('about/{theSubject}', function ($theSubject){
+    return $theSubject . " go here";
+});
+Route::get('try/{price}/{art}',function($price,$art){
+    return "The ".$art  ." have the price : ".$price;
+});
+Route::get('where', function (){
+    return redirect('about');
+});
+Route::redirect('where1','about',302);
+Route::get('try/{price?}/{art?}',function($price=null,$art=null){
+    if (!$art){
+        return "Please enter art";
+    }
+    if (!$price){
+        return "Please enter price";
+    }
+    return "The ".$art  ." have the price : ".$price;
+});
+Route::get('user/{userid}/name/{username}',function ($userid,$username){
+    return "user ID is $userid <br> user name is $username";
+}) ->where('userid','[0-9]+') ->where('username','[a-zA-Z]+');
